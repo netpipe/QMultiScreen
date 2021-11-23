df9f2b5d33f3ffb098b9a6842c5d382ad5380711
netpipe <netpipe@localhost.localdomain>
2021-11-22 22:09:00 -0700

    initial

---
+++ mainwindow.cpp
@@ -0,0 +1,56 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QProcess>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+
+    //xrandr | grep " connected"
+
+
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QProcess::execute("bash", QStringList() << "-c" << "xrandr --output HDMI-2 --brightness 1.0 --gamma 1.0:1.00:1.00"  );
+
+}
+
+void MainWindow::on_comboBox_activated(const QString &arg1)
+{
+    //qDebug() << arg1.toLatin1();
+    QProcess::execute("bash", QStringList() << "-c" << "xrandr --output HDMI-2 --gamma " + arg1.toLatin1() );
+
+            //xrandr --output "HDMI-2" --brightness 1.2
+
+
+}
+
+void MainWindow::on_brightness_sliderReleased()
+{
+    float k = ui->brightness->value();
+    k = k / 100;
+  //  ui->brightlbl->setText(QString::number(QString::number(ui->brightness->value()/100).toFloat()) );
+    QProcess::execute("bash", QStringList() << "-c" << "xrandr --output HDMI-2 --brightness " + QString::number(k) );
+
+}
+
+void MainWindow::on_brightness_valueChanged(int value)
+{
+    float k = value;
+    k = k / 100;
+    ui->brightlbl->setText(QString::number(k) );
+
+}
